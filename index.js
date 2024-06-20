eb3eb9bd66762610066d255be8b21ae94053ec11
Eugene Mathenge <eugene.mukabi@student.moringaschool.com>
2024-06-20 14:44:06 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log(introductionWithLanguage('Eugene', 'Python'));
+
+
+
+
 
 
 function introductionWithLanguageOptional(name, language = 'JavaScript') {
